5447e2192348d665f4111128561673cfe762ccce
Hridesh sharma <61771411+hridesh-net@users.noreply.github.com>
2022-10-14 03:24:22 +0530

    Assignment 3 completed

---
+++ Assignment4/kth_smallest_element.java
@@ -0,0 +1,35 @@
+package Assignment4;
+import java.io.*;
+import java.util.*;
+
+public class kth_smallest_element {
+    public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+class Solution{
+    public int kthSmallest(int[] a, int l, int r, int k) 
+    { 
+        //Your code here
+        Arrays.sort(a);
+        return a[k-1];
+    } 
+}
---
+++ Assignment4/leader_in_array.java
@@ -0,0 +1,70 @@
+package Assignment4;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class leader_in_array {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+
+        while (t-- > 0) {
+
+            // input size of array
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            // inserting elements in the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Solution obj = new Solution();
+
+            StringBuffer str = new StringBuffer();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+
+            // calling leaders() function
+            res = obj.leaders(arr, n);
+
+            // appending result to a String
+            for (int i = 0; i < res.size(); i++) {
+                str.append(res.get(i) + " ");
+            }
+
+            // printing the String
+            System.out.println(str);
+        }
+
+    }
+}
+
+class Solution {
+    // Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n) {
+        // Your code here
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        int currMax = 0;
+
+        for (int i = arr.length - 1; i > -1; i--)
+
+        {
+
+            if (arr[i] >= currMax) {
+
+                currMax = arr[i];
+
+                list.add(currMax);
+
+            }
+
+        }
+
+        Collections.reverse(list);
+
+        return list;
+    }
+}
