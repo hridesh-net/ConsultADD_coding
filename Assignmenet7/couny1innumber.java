9eae15a01d6ab289db881d37d27bdd45a5ce5ccd
Hridesh sharma <61771411+hridesh-net@users.noreply.github.com>
2022-10-19 16:21:44 +0530

    Day 7 Question 1 completed

---
+++ Assignmenet7/couny1innumber.java
@@ -0,0 +1,27 @@
+package Assignmenet7;
+import java.util.* ;
+// import java.io.*; 
+
+public class couny1innumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        Solution obj = new Solution();
+        System.out.println(obj.countOnBits(a));
+        sc.close();
+    }
+}
+
+
+class Solution {
+	public int countOnBits(int n) {
+		// Write your code here.
+        int count = 0;
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+	}
+}
